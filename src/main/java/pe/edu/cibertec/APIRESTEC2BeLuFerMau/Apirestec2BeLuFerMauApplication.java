d36b4def1bf333036651084cb4349b12b105b6a9
Mauricio51448 <mvmauriciomiki@gmail.com>
2024-04-27 00:17:59 -0500

    Iniciando pregunta 3

---
+++ src/main/java/pe/edu/cibertec/APIRESTEC2BeLuFerMau/Apirestec2BeLuFerMauApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.APIRESTEC2BeLuFerMau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apirestec2BeLuFerMauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apirestec2BeLuFerMauApplication.class, args);
+	}
+
+}
